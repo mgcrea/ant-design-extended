2d4941a1150d856cfc95f2daf87e866a60ad0cac
Olivier Louvignes <olivier@mgcrea.io>
2020-11-25 23:16:54 +0100

    feat(update): minor changes

--- src/components/checkbox/index.ts
+++ src/components/checkbox/index.ts
@@ -1,11 +1,12 @@
-export * from './Checkbox';
-export * from './CheckboxButton';
-export * from './CheckboxGroup';
+import { Checkbox as BaseCheckbox } from "./Checkbox";
+import { CheckboxButton } from "./CheckboxButton";
+import { CheckboxGroup } from "./CheckboxGroup";
 
-import {Checkbox as BaseCheckbox} from './Checkbox';
-import {CheckboxButton} from './CheckboxButton';
-import {CheckboxGroup} from './CheckboxGroup';
+export * from "./Checkbox";
+export * from "./CheckboxButton";
+export * from "./CheckboxGroup";
+
 export const Checkbox = Object.assign(BaseCheckbox, {
   Button: CheckboxButton,
-  Group: CheckboxGroup
+  Group: CheckboxGroup,
 });
